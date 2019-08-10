b77d34100972e3223f6221245daca27c48236b5d
jisnoh8 <jisnoh8@gmail.com>
2019-08-09 21:04:31 -0400

    done

--- app/data/friends.js
+++ app/data/friends.js
@@ -10,20 +10,20 @@ var friends = [
       photo:
         "https://avatarfiles.alphacoders.com/534/53498.jpg",
       scores: [4, 3, 1, 3, 5, 2, 4, 2, 2, 1]
-    }
+    },
     {
 
       name: "Charmander",
       photo:
         "https://avatarfiles.alphacoders.com/160/160978.jpg",
       scores: [3, 2, 1, 5, 4, 3, 2, 4, 5, 2]
-    }
+    },
     {
       name: "Eevee",
       photo:
         "https://avatarfiles.alphacoders.com/160/160963.jpg",
       scores: [2, 4, 5, 4, 3, 1, 5, 2, 4, 5]
-    }
+    },
     {
       name: "Dragonite",
       photo:
