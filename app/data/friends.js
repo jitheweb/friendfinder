977856b20a0073370ccf9661d84a377436b4063f
jisnoh8 <jisnoh8@gmail.com>
2019-08-09 21:01:10 -0400

    done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,34 @@
 var friends = [
     {
-      name: "Blake Lively",
+      name: "Pikachu",
       photo:
-        "https://media.vanityfair.com/photos/560c07690404673e0de66e1d/master/w_960,c_limit/blake-lively-end-of-preserve-01.jpg",
+        "https://i.pinimg.com/originals/74/25/16/7425165d0b88a22311aac696cfeace24.jpg",
       scores: [4, 3, 4, 2, 4, 3, 5, 3, 5, 4]
     },
     {
-      name: "Kate Upton",
+      name: "Squirtle",
       photo:
-        "https://m.media-amazon.com/images/M/MV5BMTk3MTA2ODQ1MF5BMl5BanBnXkFtZTcwMjkwMDYxNw@@._V1_SY1000_CR0,0,666,1000_AL_.jpg",
-      scores: [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+        "https://avatarfiles.alphacoders.com/534/53498.jpg",
+      scores: [4, 3, 1, 3, 5, 2, 4, 2, 2, 1]
+    }
+    {
+
+      name: "Charmander",
+      photo:
+        "https://avatarfiles.alphacoders.com/160/160978.jpg",
+      scores: [3, 2, 1, 5, 4, 3, 2, 4, 5, 2]
+    }
+    {
+      name: "Eevee",
+      photo:
+        "https://avatarfiles.alphacoders.com/160/160963.jpg",
+      scores: [2, 4, 5, 4, 3, 1, 5, 2, 4, 5]
+    }
+    {
+      name: "Dragonite",
+      photo:
+        "https://avatarfiles.alphacoders.com/767/76719.jpg",
+      scores: [3, 4, 2, 4, 5, 3, 3, 5, 4, 1]
     }
   ];
   module.exports = friends;
